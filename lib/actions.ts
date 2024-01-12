1253b4ffd14abf2a5eec7e53be307e188c04c4c1
blixt <teodor.blixt@gmail.com>
2024-01-12 11:32:51 +0100

    env variables

--- lib/actions.ts
+++ lib/actions.ts
@@ -9,8 +9,8 @@ export async function newQuestion(v: z.infer<typeof formSchema>) {
   const cookieStore = cookies();
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
